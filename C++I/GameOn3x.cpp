0526eaf93750e322c6c8b421d8f251e68f2912f3
Amoh Eric <105559568+amoheric@users.noreply.github.com>
2024-01-20 12:39:33 -0500

    Add files via upload
    Beginners C++ 1

---
+++ C++I/GameOn3x.cpp
@@ -0,0 +1,38 @@
+/*
+Eric Amoh Adjei
+02/15/2023
+Assignment: Game On!
+*/
+
+// Game Over to Game On
+// My first C++ program
+
+
+// including iostream directive tells the preprocessor to include other contents file.
+
+#include <iostream>
+
+
+// with the include String, we can not display a string output or alphabetical characters.
+
+#include <string>
+
+
+// using namespace std to avoid std:: cout repetitions in our code
+
+using namespace std;
+
+
+//funtions that returns an integer, starting point
+
+int main() 
+{
+	
+	cout << " Game On " << endl << endl; // using the double << endl << endl; to create a space between my output
+
+    cout << " This is my first C++ " << endl; // ending my output line of statement
+
+
+	return 0; // to close the console by pressing any key when debuggong is done.
+
+}
---
+++ C++I/Learning Functions Header.h
@@ -0,0 +1,43 @@
+#pragma once
+// including iostream directive tells the preprocessor to include other contents file.
+#include <iostream>
+
+// with the include String, we can not display a string output or alphabetical characters.
+#include <string>
+
+// windows.h library allow coders to do more like adding delays like sleep for some seconds
+#include <windows.h>
+
+//including my locally made h file set funtion
+//#include "Learning Functions Header.h"
+
+//declaration of function to be called
+
+
+int Home();//function definition for main menu
+
+
+// this is a funtion with Arrays
+void Garage(int myCars[3]);
+
+
+//second funtion code to be executed
+void Counting();
+
+
+// this is a funtion with a return
+int multi(int num3, int num4);
+
+
+// funtion with Parameters
+void sumof(int num1, int num2);
+
+
+// this is a funtion with a decimal
+double deci(double z, double y);
+
+
+// this is a funtion with a return values
+int sumof(int x);
+
+
