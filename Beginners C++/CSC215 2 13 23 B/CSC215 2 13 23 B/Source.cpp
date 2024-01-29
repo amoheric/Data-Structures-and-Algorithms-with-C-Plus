0548504013bfc0cc8a50b5588ff471e44c91fe08
Amoh Eric <105559568+amoheric@users.noreply.github.com>
2024-01-28 22:27:44 -0500

    Add files via upload

---
+++ Beginners C++/CSC215 2 13 23 B/CSC215 2 13 23 B/Source.cpp
@@ -0,0 +1,25 @@
+// program 2
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int num1, num2;
+	double dec1, dec2;
+
+	cout << "MATH" << endl;
+
+	cout << "enter 1st number" << endl;
+	cin >> num1;
+	cout << "enter 2nd number" << endl;
+	cin >> num2;
+
+	cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+
+
+
+
+	return 0;
+
+}
