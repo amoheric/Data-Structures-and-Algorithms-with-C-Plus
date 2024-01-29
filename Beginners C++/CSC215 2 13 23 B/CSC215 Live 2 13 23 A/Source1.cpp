94bf80c70acd717483bfd3342f6a63c335c7c559
Amoh Eric <105559568+amoheric@users.noreply.github.com>
2024-01-28 22:33:07 -0500

    Add files via upload

---
+++ Beginners C++/CSC215 2 13 23 B/CSC215 Live 2 13 23 A/Source1.cpp
@@ -0,0 +1,57 @@
+// comments - 1 line
+
+/*
+multi 
+line 
+comment
+*/
+//include libraries
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() // function that returns an integer, starting point
+{
+	// variable declarations
+	int number = 3;// integer with initialization
+	int guess = 0;
+	number = rand() % 5;
+	cout << "random # = " << number << endl;
+	cout << "please enter a guess ";
+	cin >> guess;
+	cout << "You guessed " << guess << endl;
+
+	if (guess < number)
+	{
+		cout << "guess is too low" << endl;
+	}
+	else if (guess == number)
+	{
+		cout << "you guessed right" << endl;
+	}
+	else
+	{
+		cout << "guess is too high" << endl;
+	}
+
+	switch (guess)
+	{
+	case 1:
+		cout << "you chose a low number" << endl;
+		break;
+	case 3:
+		cout << "you picked a lucky number" << endl;
+		break;
+	case 7:
+		cout << "you picked a high number" << endl;
+		break;
+	default:
+		cout << "your number is nothing special" << endl;
+	}
+	
+	return 0;
+}
+
+
+
